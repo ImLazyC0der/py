d7fc3fa0c0a8d315b53b5b3a1312e0dd3a080ba9
Chethan B M <75664696+Chethanbm2721@users.noreply.github.com>
2021-10-04 20:56:26 +0530

    Create FilterCheckbox.js
    adde component for filter the item

---
+++ other_projects/e-commerce-app/src/components/FilterCheckbox.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { CustomInput } from 'reactstrap'
+
+const FilterCheckbox = ({id,name,checked,onChange,label}) => {
+  return (
+    <div>
+      <CustomInput
+      type="checkbox"
+      id={id}
+      name={name}
+      checked={checked}
+      onChange={onChange}
+      label={label}
+      />
+    </div>
+  )
+}
+
+export default FilterCheckbox;
