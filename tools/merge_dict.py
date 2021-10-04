6c88e71abee7e5fb3ef6a00726877ebac373121d
ImLazyCoder-eng <73062651+ImLazyCoder-eng@users.noreply.github.com>
2021-10-04 20:27:39 +0530

    Revert "dummy one"

---
+++ tools/merge_dict.py
@@ -0,0 +1,6 @@
+dictionary1 = {"name": "Joy", "age": 25}
+dictionary2 = {"name": "Joy", "city": "New York"}
+
+merged_dict = {**dictionary1, **dictionary2}
+
+print("Merged dictionary:", merged_dict)
